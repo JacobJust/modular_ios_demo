4ce1e04717a7f478178ecfaedccc052be0d85642
Jacob Just <jacob.just@schneider-electric.com>
2019-01-31 11:08:09 +0100

    initial ios app

---
+++ ios_app/module demo/module demo/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  module demo
+//
+//  Created by Jacob Just on 31/01/2019.
+//  Copyright © 2019 Jacob Just. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
---
+++ ios_app/module demo/module demoTests/module_demoTests.swift
@@ -0,0 +1,34 @@
+//
+//  module_demoTests.swift
+//  module demoTests
+//
+//  Created by Jacob Just on 31/01/2019.
+//  Copyright © 2019 Jacob Just. All rights reserved.
+//
+
+import XCTest
+@testable import module_demo
+
+class module_demoTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
